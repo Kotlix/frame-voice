800608ef8da2fc7e9a48bc13c74b4d415bdc6e6d
alexgureev <amgureev@edu.hse.ru>
2025-05-18 13:15:35 +0300

    KTX-41 add ResponseStatus

--- frame-voice-server/src/main/kotlin/ru/kotlix/frame/voice/server/exception/ServerNotFoundException.kt
+++ frame-voice-server/src/main/kotlin/ru/kotlix/frame/voice/server/exception/ServerNotFoundException.kt
@@ -1,4 +1,8 @@
 package ru.kotlix.frame.voice.server.exception
 
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.ResponseStatus
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 class ServerNotFoundException(name: String, region: String) :
     RuntimeException("Server $name $region is not found")
